3c9e51be2100cfcca3416d1188c5bfb5361293ee
M-1357029 <0357029@student.rocvantwente.nl>
2026-04-01 11:07:29 +0200

    Add default client tasks to the registration process

--- src/services/authService.js
+++ src/services/authService.js
@@ -10,6 +10,115 @@ const comparePassword = async (password, hash) => {
     return bcryptjs.compare(password, hash);
 };
 
+const DEFAULT_CLIENT_TASKS = [
+    {
+        title: 'Ga 5 minuten wandelen',
+        description: 'Zelf 5 minuten wandelen, startknop aan, per minuut 1 ster verdienen',
+        image_url: 'https://photos1.blogger.com/x/blogger/387/3031/1600/818020/Wandelen(1).jpg',
+        points: 5,
+        category_name: 'Lichaamsfuncties / Meedoen',
+        layer: 'zelf',
+    },
+    {
+        title: 'Activiteit naar keuze',
+        description: 'Kies zelf een activiteit, in overleg met zorgpersoneel',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/kiezen.png',
+        points: 5,
+        category_name: 'Lichaamsfuncties / Meedoen',
+        layer: 'zelf',
+    },
+    {
+        title: 'Handoefeningen doen',
+        description: 'Voer handoefeningen uit aan de hand van uitlegfilmpje, startknop aan, per minuut 1 ster',
+        image_url: 'https://www.sclera.be/resources/pictos/handen%20klappen.png',
+        points: 5,
+        category_name: 'Lichaamsfuncties / Meedoen',
+        layer: 'zelf',
+    },
+    {
+        title: 'Stoeloefeningen doen',
+        description: 'Voer stoeloefeningen uit aan de hand van filmpje',
+        image_url: 'https://www.sclera.be/resources/pictos/zitten%20aandachtspunten.png',
+        points: 5,
+        category_name: 'Lichaamsfuncties / Meedoen',
+        layer: 'zelf',
+    },
+    {
+        title: 'Zelf aankleden',
+        description: 'Zelfstandig aankleden',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/aankleden.png',
+        points: 3,
+        category_name: 'Dagelijks Functioneren / Kwaliteit van Leven',
+        layer: 'zelf',
+    },
+    {
+        title: 'Zelfstandig wassen',
+        description: 'Zelfstandig jezelf wassen',
+        image_url: 'https://easyauti.nl/product/losse-picto-jezelf-wassen/',
+        points: 3,
+        category_name: 'Dagelijks Functioneren / Kwaliteit van Leven',
+        layer: 'zelf',
+    },
+    {
+        title: 'Zelfstandig ontbijt klaarmaken',
+        description: 'Een ontbijt zelfstandig klaarmaken',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/jezelf-wassen.png',
+        points: 3,
+        category_name: 'Dagelijks Functioneren / Kwaliteit van Leven',
+        layer: 'zelf',
+    },
+    {
+        title: 'Medicatie innemen',
+        description: 'Medicatie innemen onder begeleiding of zelfstandig',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/medicijnen-innemen-pillen-en-flesjes.png',
+        points: 3,
+        category_name: 'Dagelijks Functioneren / Kwaliteit van Leven',
+        layer: 'zelf',
+    },
+    {
+        title: 'Met een vriend bellen',
+        description: 'Bel zelf een vriend op',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/bellen-1.png',
+        points: 4,
+        category_name: 'Zingeving / Mentaal Welbevinden',
+        layer: 'sociaal_netwerk',
+    },
+    {
+        title: 'Met familie op pad',
+        description: 'Ga met familie op uit',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2022/06/op-bezoek-1.png',
+        points: 4,
+        category_name: 'Zingeving / Mentaal Welbevinden',
+        layer: 'sociaal_netwerk',
+    },
+    {
+        title: 'Met vrijwilliger naar muziek',
+        description: 'Luister naar muziek met vrijwilliger',
+        image_url: 'https://easyauti.nl/wp-content/uploads/2023/10/muziek-10x10cm-600x600.png',
+        points: 4,
+        category_name: 'Zingeving / Mentaal Welbevinden',
+        layer: 'sociaal_netwerk',
+    },
+];
+
+const insertDefaultClientTasks = async (transaction, clientId) => {
+    for (const task of DEFAULT_CLIENT_TASKS) {
+        await new sql.Request(transaction)
+            .input('id', sql.UniqueIdentifier, uuidv4())
+            .input('client_id', sql.UniqueIdentifier, clientId)
+            .input('title', sql.NVarChar(200), task.title)
+            .input('description', sql.NVarChar(1000), task.description)
+            .input('image_url', sql.NVarChar(sql.MAX), task.image_url)
+            .input('points', sql.Int, task.points)
+            .input('category_name', sql.NVarChar(200), task.category_name)
+            .input('layer', sql.NVarChar(20), task.layer)
+            .query(`
+                INSERT INTO dbo.Tasks (id, client_id, title, description, image_url, points, category_name, layer, is_active)
+                VALUES (@id, @client_id, @title, @description, @image_url, @points, @category_name, @layer, 1)
+            `);
+    }
+};
+
 const register = async (email, password, fullName, role) => {
     const pool = await getConnection();
     const transaction = new sql.Transaction(pool);
@@ -56,6 +165,8 @@ const register = async (email, password, fullName, role) => {
                 .query(
                     'INSERT INTO dbo.Clients (id, user_id) VALUES (@id, @user_id)'
                 );
+
+            await insertDefaultClientTasks(transaction, clientId);
         }
 
         await transaction.commit();
